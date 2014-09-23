3df1b847e0db66ef8c962b73ef63bda9d1916f45
codywu <codywu@zheng-exp.lab>
2014-09-23 22:06:16 +0800

    Factor conversion from WebPreferences to CefBrowserSettings out as individual function

--- WebSessionImpl.cpp
+++ WebSessionImpl.cpp
@@ -15,44 +15,56 @@
 
 using namespace Awesomium;
 
-WebSessionImpl::WebSessionImpl(const WebString& path,
-                               const WebPreferences& prefs)
-        : path_(path), prefs_(prefs)
+namespace {
+
+CefBrowserSettings to_cef_browser_settings(const WebPreferences& prefs)
 {
-    settings_.javascript = to_cef_state_t(prefs.enable_javascript);
-    settings_.plugins = to_cef_state_t(prefs.enable_plugins);
-    settings_.local_storage = to_cef_state_t(prefs.enable_local_storage);
-    settings_.databases = to_cef_state_t(prefs.enable_databases);
-    settings_.application_cache = to_cef_state_t(prefs.enable_app_cache);
-    settings_.webgl = to_cef_state_t(prefs.enable_web_gl);
-    settings_.web_security = to_cef_state_t(prefs.enable_web_security);
-    settings_.remote_fonts = to_cef_state_t(prefs.enable_remote_fonts);
-    settings_.accelerated_compositing = to_cef_state_t(
+    CefBrowserSettings browser_settings;
+    browser_settings.javascript = to_cef_state_t(prefs.enable_javascript);
+    browser_settings.plugins = to_cef_state_t(prefs.enable_plugins);
+    browser_settings.local_storage = to_cef_state_t(prefs.enable_local_storage);
+    browser_settings.databases = to_cef_state_t(prefs.enable_databases);
+    browser_settings.application_cache = to_cef_state_t(prefs.enable_app_cache);
+    browser_settings.webgl = to_cef_state_t(prefs.enable_web_gl);
+    browser_settings.web_security = to_cef_state_t(prefs.enable_web_security);
+    browser_settings.remote_fonts = to_cef_state_t(prefs.enable_remote_fonts);
+    browser_settings.accelerated_compositing = to_cef_state_t(
             prefs.enable_gpu_acceleration);
     if (!prefs.user_stylesheet.IsEmpty()) {
-        /// @todo Refer to http://magpcss.org/ceforum/apidocs3/projects/(default)/_cef_browser_settings_t.html#user_style_sheet_location
+        /// @todo Refer to http://magpcss.org/ceforum/apidocs3/projects/(default)/_cef_browser_browser_settingst.html#user_style_sheet_location
     }
-    if (!prefs.default_encoding.IsEmpty())
-        update_cef_string_t(&settings_.default_encoding,
+    if (!prefs.default_encoding.IsEmpty()) {
+        update_cef_string_t(&browser_settings.default_encoding,
                 prefs.default_encoding);
-    settings_.image_shrink_standalone_to_fit = to_cef_state_t(
+    }
+    browser_settings.image_shrink_standalone_to_fit = to_cef_state_t(
             prefs.shrink_standalone_images_to_fit);
-    settings_.image_loading = to_cef_state_t(prefs.load_images_automatically);
-    settings_.javascript_open_windows = to_cef_state_t(
+    browser_settings.image_loading = to_cef_state_t(
+            prefs.load_images_automatically);
+    browser_settings.javascript_open_windows = to_cef_state_t(
             prefs.allow_scripts_to_open_windows);
-    settings_.javascript_close_windows = to_cef_state_t(
+    browser_settings.javascript_close_windows = to_cef_state_t(
             prefs.allow_scripts_to_close_windows);
-    settings_.javascript_access_clipboard = to_cef_state_t(
+    browser_settings.javascript_access_clipboard = to_cef_state_t(
             prefs.allow_scripts_to_access_clipboard);
-    settings_.universal_access_from_file_urls = to_cef_state_t(
+    browser_settings.universal_access_from_file_urls = to_cef_state_t(
             prefs.allow_universal_access_from_file_url);
-    settings_.file_access_from_file_urls = to_cef_state_t(
+    browser_settings.file_access_from_file_urls = to_cef_state_t(
             prefs.allow_file_access_from_file_url);
     /// @note unused variables in WebPreferences
     //  enable_dart, enable_web_audio, enable_smooth_scrolling, proxy_config,
     //  accept_language, accept_charset, default_encoding,
     //  allow_running_insecure_content
-    
+    return browser_settings;
+}
+
+}
+
+WebSessionImpl::WebSessionImpl(const WebString& path,
+                               const WebPreferences& prefs)
+        : path_(path), prefs_(prefs)
+{
+    settings_ = to_cef_browser_settings(prefs);
     /// Create cookie manager for use with this session
     //WebCoreImpl* web_core = dynamic_cast<WebCoreImpl*>(WebCore::instance());
     //CefString cache_path = web_core->settings().cache_path;
